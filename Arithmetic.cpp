d969fb81cfedb3215d31a214bbb8e1f6ffc05383
SUNCHAOYI923 <3187756748@qq.com>
2025-05-01 13:42:55 +0800

    Add Integer Class and Combination Class.

---
+++ Arithmetic.cpp
@@ -0,0 +1,34 @@
+template <typename T>
+class Z
+{
+    T x;static const int MOD = 1e9 + 7;
+    Z <T> qpow (Z x,T y)
+    {
+        Z <T> res (1);
+        while (y)
+        {
+            if (y & 1) res *= x;
+            x *= x;
+            y >>= 1;
+        }
+        return res;
+    }
+    public :
+    Z () : x(0) {}
+    Z (T x) : x (x) {}
+    Z & operator = (T o) {x = o;return *this;}
+    Z & operator += (Z o) {x = (x + o.x + MOD) % MOD;return *this;}
+    Z & operator -= (Z o) {x = (x - o.x + MOD) % MOD;return *this;}
+    Z & operator *= (Z o) {x = 1ll * o.x * x % MOD;return *this;}
+    Z & operator ^= (Z o) {x = qpow (x,o.x).x;return *this;}
+    Z & operator /= (Z o) {*this *= o ^ (MOD - 2);return *this;}
+    friend Z operator + (Z x,Z y) {return x += y;}
+    friend Z operator - (Z x,Z y) {return x -= y;}
+    friend Z operator * (Z x,Z y) {return x *= y;}
+    friend Z operator / (Z x,Z y) {return x /= y;}
+    friend Z operator ^ (Z x,Z y) {return x ^= y;}
+    friend bool operator == (Z x,Z y){ return x.x == y.x;}
+    friend bool operator != (Z x,Z y){ return x.x != y.x;}
+    friend istream& operator >> (istream& is, Z& o) {T val;is >> val;o = Z(val);return is;}
+    friend ostream& operator << (ostream &os, const Z &z) {return os << z.x;}
+};
---
+++ Combination.cpp
@@ -0,0 +1,21 @@
+template <typename T>
+class COM
+{
+	int n;static const int MOD = 1e9 + 7;
+	vector <Z <T>> fac,inv;
+	public : 
+	COM (int n) : n (n),fac (n + 1),inv (n + 1)
+	{
+		fac[0] = inv[0] = 1;
+        for (int i = 1;i <= n;++i) fac[i] = fac[i - 1] * Z <T> (i);
+        inv[n] = fac[n] ^ MOD - 2;
+        for (int i = n - 1;i;--i) inv[i] = inv[i + 1] * Z <T> (i + 1);
+	}
+    Z <T> f (int x) {return fac[x];}
+    Z <T> inv_f (int x) {return inv[x];}
+    Z <T> comb (int x,int y)
+    {
+        if (y < 0 || y > x) return Z <T> (0);
+        else return fac[x] * inv[x - y] * inv[y];   
+    }
+};
