52e871c6d3d5d646b05ea11535d5d96ed27ab30a
SUNCHAOYI923 <3187756748@qq.com>
2025-06-05 17:22:29 +0800

    Modify Linear-Basis.cpp.

--- Linear-Basis.cpp
+++ Linear-Basis.cpp
@@ -2,17 +2,22 @@ template <typename T>
 class Basis
 {
     int n,lg;
-    vector <T> p,ex,nw;
+    vector <T> p,ex,nw;vector <vector <int>> id;
     public :
-    Basis (int n,int lg) : n (n),lg (lg),p (lg + 1,0),nw (lg + 1,0) {}
-    void modify (T x)
+    Basis (int n,int lg) : n (n),lg (lg),p (lg + 1,0),nw (lg + 1,0),id (lg + 1) {}
+    void modify (T x,int idx)
     {
+        vector <int> cur (n + 1,0);cur[idx] = 1;
         for (int i = lg;~i;--i)
         {
             if (!((1ll << i) & x)) continue;
-            if (!p[i]) {p[i] = x;break;}
-            else x ^= p[i]; 
-        }
+            if (!p[i]) {p[i] = x;id[i] = cur;break;}
+            else 
+            {
+                x ^= p[i];
+                for (int j = 1;j <= n;++j) cur[j] ^= id[i][j];
+            }
+        }   
     }
     T get_max ()
     {
@@ -47,5 +52,26 @@ class Basis
         for (int j = 0;j < sz;++j)
             if (k >> j & 1) ans ^= nw[ex[j]];
         return ans;
+    }
+    bool find (T x)
+    {
+        for (int i = lg;~i;--i)
+        {
+            if (!(x >> i & 1)) continue;
+            if (!p[i]) return false;
+            x ^= p[i];
+        }
+        return true;
+    }
+    vector <int> getid (T x) // if find (x) == 1
+    {
+        vector <int> res (n + 1,0);
+        for (int i = lg;~i;--i)
+        {
+            if (!(x >> i & 1)) continue;
+            x ^= p[i];
+            for (int j = 1; j <= n; ++j) res[j] ^= id[i][j];
+        }
+        return res;
     }
 };
