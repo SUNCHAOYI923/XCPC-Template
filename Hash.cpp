ddc386c1a9ca4cfdacedee3eff783c6b6345832d
SUNCHAOYI923 <3187756748@qq.com>
2025-06-06 17:18:24 +0800

    Fix errors in Hash.cpp.

--- Hash.cpp
+++ Hash.cpp
@@ -3,6 +3,7 @@ class Hash
 {
     using u64 = unsigned long long;
     vector <u64> pw,hsh;
+    public:
     Hash (char *s)
     {
         int n = strlen (s);
@@ -11,7 +12,6 @@ class Hash
         for (int i = 1; i <= n;++i) pw[i] = pw[i - 1] * base,hsh[i] = hsh[i - 1] * base + s[i - 1];
         return hsh;
     }
-    public:
     u64 get (int l,int r) {return hsh[r] - hsh[l - 1] * pw[r - l + 1];}
     u64 link (int l1,int r1,int l2,int r2) {return get (l1,r1) * pw[r2 - l2 + 1] + get (l2,r2);}
     bool same (int l1,int r1,int l2,int r2) {return get (l1,r1) == get (l2,r2);}
