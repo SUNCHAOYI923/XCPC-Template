acda53638162bd308476afdaf144b8eb0bdedc51
SUNCHAOYI923 <3187756748@qq.com>
2025-06-06 17:24:47 +0800

    Fix errors in Hash.cpp.

--- Hash.cpp
+++ Hash.cpp
@@ -8,9 +8,8 @@ class Hash
     {
         int n = strlen (s);
         pw.assign (n,0),hsh.assign (n,0);
-        pow[0] = 1;hsh[0] = 0;
+        pw[0] = 1;hsh[0] = 0;
         for (int i = 1; i <= n;++i) pw[i] = pw[i - 1] * base,hsh[i] = hsh[i - 1] * base + s[i - 1];
-        return hsh;
     }
     u64 get (int l,int r) {return hsh[r] - hsh[l - 1] * pw[r - l + 1];}
     u64 link (int l1,int r1,int l2,int r2) {return get (l1,r1) * pw[r2 - l2 + 1] + get (l2,r2);}
