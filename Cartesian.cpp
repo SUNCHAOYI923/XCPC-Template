14b81b1647728c8daafb4265fe3252adb7382a6d
SUNCHAOYI923 <3187756748@qq.com>
2025-08-13 21:00:05 +0800

    using stack <int> to rewrite Cartesian.cpp.

--- Cartesian.cpp
+++ Cartesian.cpp
@@ -1,9 +1,11 @@
 //heap-order on values and BST-order on keys
+stack <int> s;
+vector <int> L (n + 1,0),R (n + 1,0);
 for (int i = 1;i <= n;++i)
 {
-    int p = top;
-    while (p && a[s[p]] > a[i]) --p;
-    if (p) R[s[p]] = i;
-    if (p < top) L[i] = s[p + 1];
-    s[++p] = i;top = p;
+    int la = -1;
+    while (!s.empty () && a[s.top ()] > a[i]) la = s.top (),s.pop ();
+    if (!s.empty ()) R[s.top ()] = i;
+    if (la != -1) L[i] = la;
+    s.push (i);
 }
