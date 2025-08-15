6ec77452ec44ed806f82b74022793791fb69eb53
SUNCHAOYI923 <3187756748@qq.com>
2025-08-15 13:27:42 +0800

    Modify PDF.

--- Cmp.sh
+++ Cmp.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 for ((i=1; ; i++)); do
-    ./gen > input.txt           # 生成数据
-    ./code < input.txt > output1.txt  # 你的代码运行
-    ./std < input.txt > output2.txt   # 标准答案运行
-    if diff output1.txt output2.txt; then  # 比较输出
+    ./gen > input.txt           # generator
+    ./code < input.txt > output1.txt  # your code
+    ./std < input.txt > output2.txt   # std code
+    if diff output1.txt output2.txt; then  # compare
         echo "Test $i: Accepted"
     else
         echo "Test $i: Wrong Answer"
