62760754c26e90c7b0e26b0a2cec4262aed1047a
SUNCHAOYI923 <3187756748@qq.com>
2025-11-04 16:17:10 +0800

    Modify Tarjan.

--- Tarjan.cpp
+++ Tarjan.cpp
@@ -12,10 +12,10 @@ class Tarjan
             int v = to[i];
             if (!dfn[v])
             {
-                tarjan (v,i);
+                tarjan (v);
                 low[u] = min (low[u],low[v]);
             }
-            else if (i != (la ^ 1) && !scc[v]) low[u] = min (low[u],dfn[v]);
+            else if (!scc[v]) low[u] = min (low[u],dfn[v]);
         }
         if (low[u] == dfn[u])
         {
@@ -32,15 +32,11 @@ class Tarjan
     Tarjan (int n,int m) : 
         n (n),m (m),head (n + 2,0),low (n + 1,0),dfn (n + 1,0),scc (n + 1,0),
         to (2 * m + 2,0),nxt (2 * m + 2,0) {cnt = 1;times = scc_cnt = 0;}
-    void add (int u,int v) // Note that the bidirectional edges
-    {
-        to[++cnt] = v;nxt[cnt] = head[u];head[u] = cnt;
-        to[++cnt] = u;nxt[cnt] = head[v];head[v] = cnt;
-    }
+    void add (int u,int v) {to[++cnt] = v;nxt[cnt] = head[u];head[u] = cnt;}
     auto calc ()
     {
         for (int i = 1;i <= n;++i)
-            if (!dfn[i]) tarjan (i,-1);
+            if (!dfn[i]) tarjan (i);
         return scc;
     }
 };
