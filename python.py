9f4cfa0807df9376c33911e2efdf02e1f120795c
SUNCHAOYI923 <3187756748@qq.com>
2026-01-28 00:44:28 +0800

    Add python -- sys for deeper recursion.

--- python.py
+++ python.py
@@ -1,4 +1,4 @@
 import sys
 sys.set_int_max_str_digits (value) # set big number
-
+sys.setrecursionlimit (deep) # set deeper recursion
 lst = sorted (lst,key = lambda x : x[1]) # sort by the second element
