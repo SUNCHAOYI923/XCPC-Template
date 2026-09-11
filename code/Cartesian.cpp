129ab63f1a18b0006e831d4b7618018ad18b8cbe
jrzlw <124090875@link.cuhk.edu.cn>
2026-09-11 20:15:58 +0800

    Cartesian Tree Complement

--- code/Cartesian.cpp
+++ code/Cartesian.cpp
@@ -1,6 +1,7 @@
-//heap-order on values and BST-order on keys
+//heap-order(root is min) on values and BST-order on keys
 stack <int> s;
 vector <int> L(n + 1, 0), R(n + 1, 0);
+vector <int> ql(n + 1, 0), qr(n + 1, 0);
 for (int i = 1; i <= n; ++i)
 {
     int la = -1;
@@ -9,3 +10,12 @@ for (int i = 1; i <= n; ++i)
     if (la != -1) L[i] = la;
     s.push(i);
 }
+int Rt = 0;
+while (!s.empty()) Rt = s.top(), s.pop();
+auto dfs = [&](auto self, int u, int l, int r)->void
+{
+    ql[u] = l; qr[u] = r;
+    if(L[u])self(self, L[u], l, u - 1);
+    if(R[u])self(self, R[u], u + 1, r);
+};
+dfs(dfs, Rt, 1, n);
