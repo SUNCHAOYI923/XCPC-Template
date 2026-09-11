6a9b57fb42194a8370717b9164c9c1ea4b557398
jrzlw <124090875@link.cuhk.edu.cn>
2026-09-11 21:25:39 +0800

    Cartesian Tree update

--- code/Cartesian.cpp
+++ code/Cartesian.cpp
@@ -5,7 +5,7 @@ vector <int> ql(n + 1, 0), qr(n + 1, 0);
 for (int i = 1; i <= n; ++i)
 {
     int la = -1;
-    while (!s.empty() && a[s.top()] > a[i]) la = s.top(), s.pop();
+    while (!s.empty() && a[s.top()] < a[i]) la = s.top(), s.pop();
     if (!s.empty()) R[s.top()] = i;
     if (la != -1) L[i] = la;
     s.push(i);
